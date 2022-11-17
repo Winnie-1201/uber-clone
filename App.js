71f0232f31608fc2f33b9e54054179fa45b2d923
Winnie <luwinnie12@gmail.com>
2022-11-16 19:05:06 -0500

    setting up the homepage

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
-
+import { SafeAreaProvider } from "react-native-safe-area-context";
 // 1. Set up Redux
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>
+      {/* <View style={styles.container}>
         <Text>Let's build Uber!</Text>
         <StatusBar style="auto" />
-      </View>
+      </View> */}
     </Provider>
   );
 }
